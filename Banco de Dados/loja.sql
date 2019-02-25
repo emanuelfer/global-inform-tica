5a4bb792ed38e0150df6e649dded5ccad8c2b244
Emanuel <emanuelstrato999@gmail.com>
2019-02-25 19:56:30 -0300

    atualização do bd

--- Banco de Dados/loja.sql
+++ Banco de Dados/loja.sql
@@ -65,7 +65,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (1,'ferreira','ferreira@gmail.com','55555555555','8d13ed81f15ff53688df90dd38cbd6d6');
+INSERT INTO `cliente` VALUES (0,'daniel','daniel@gmail.com','22222222222','aa47f8215c6f30a0dcdb2a36a9f4168e'),(1,'ferreira','ferreira@gmail.com','55555555555','8d13ed81f15ff53688df90dd38cbd6d6'),(2,'gabriel','gabriel@gmail.com','11111111111','647431b5ca55b04fdf3c2fce31ef1915'),(3,'daniel','daniel@gmail.com','22222222222','aa47f8215c6f30a0dcdb2a36a9f4168e');
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,6 +95,34 @@ LOCK TABLES `disciplina` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `lista_desejo`
+--
+
+DROP TABLE IF EXISTS `lista_desejo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lista_desejo` (
+  `idProduto` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  PRIMARY KEY (`idProduto`,`idUsuario`),
+  KEY `idProduto_idx` (`idProduto`),
+  KEY `idUsuario_idx` (`idUsuario`),
+  CONSTRAINT `idProduto` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `idUsuario` FOREIGN KEY (`idUsuario`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lista_desejo`
+--
+
+LOCK TABLES `lista_desejo` WRITE;
+/*!40000 ALTER TABLE `lista_desejo` DISABLE KEYS */;
+INSERT INTO `lista_desejo` VALUES (6,1),(7,1),(11,2),(12,2),(16,0);
+/*!40000 ALTER TABLE `lista_desejo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `produto`
 --
 
@@ -103,13 +131,13 @@ DROP TABLE IF EXISTS `produto`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `produto` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(20) DEFAULT NULL,
+  `nome` varchar(25) DEFAULT NULL,
   `descricao` varchar(50) DEFAULT NULL,
   `preco` double DEFAULT NULL,
   `especificacao` varchar(156) DEFAULT NULL,
   `foto` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,7 +146,7 @@ CREATE TABLE `produto` (
 
 LOCK TABLES `produto` WRITE;
 /*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-INSERT INTO `produto` VALUES (6,'LG Optimus Prime 2','Celular Top de Linha',1250,'Celular transformer','product-3.jpg'),(7,'Sony Ericsen','Ultima Geração da Sony',659,'Muito bom.','product-4.jpg');
+INSERT INTO `produto` VALUES (6,'LG Prime Y','Celular Top de Linha',1250,'Celular transformer','product-3.jpg'),(7,'Sony Experience Z','Ultima Geração da Sony',659,'Muito bom.','product-4.jpg'),(11,'Roteador Dell XS','Roteador com capacidade de roteamento num raio de ',899.99,'Roteador Wireless','images (5).jpg'),(12,'Mac Book Air','Com um design fino e leve, o Apple MacBook Air 201',5199,'13\'\' Led HD, 8GB, SSD 128, Intel Core I5 e macOS Sierra','42995703_1GG.jpg'),(13,'Computador Dell X1129','Processador\r\nProcessador Intel® Pentium® Silver N5',2267,'Notebook de 15 polegadas com recursos essenciais para ajudar você nas tarefas diárias, incluindo processador Intel® Pentium®, CinemaStream, CinemaSound e te','notebooks-xps-15-9570-laptop-pdp-design-mod4a.jpg'),(14,'Iphone X','É só começar a usar o iPhone 6S para perceber como',1999,'32GB Rosê Tela Retina HD 4,7','129251759_1GG.png'),(15,'Nokia Lumia 920','Com um processador Qualcomm MSM8960 Snapdragon S4 ',243,'Desbloqueado com 4.5 \"tela Capacitiva Dual core 32G ROM + 1G RAM Frete Grátis','product-2.jpg'),(16,'Samsung Galaxy S','Qualidade Samsung. Nunca trava!',2000,'Produto muito bom bom.','product-3.jpg');
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -131,4 +159,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-07 18:37:39
+-- Dump completed on 2019-02-25 19:55:45
